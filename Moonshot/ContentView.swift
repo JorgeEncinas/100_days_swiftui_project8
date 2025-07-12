736b9bd9096082fdb69d49f6d3d4a2ff59ba8be3
Jorge Encinas <jorgecarlosencinas@gmail.com>
2025-07-12 13:24:05 -0700

    Decoding extension

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,7 +8,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        GridLayoutView3()
+        //GridLayoutView3()
+        DecodableJSONView()
     }
 }
 
---
+++ Moonshot/Views/DecodableJSONView.swift
@@ -0,0 +1,34 @@
+//
+//  DecodableJSONView.swift
+//  Moonshot
+//
+//  Created by Jorge Encinas on 7/12/25.
+//
+import SwiftUI
+
+// Loading some Codable Data
+
+struct Astronaut: Codable, Identifiable {
+    let id : String
+    let name : String
+    let description : String
+}
+
+// To convert the JSON `astronauts.json` into a DICTIONARY of Astronaut instances
+// we need to use `Bundle` to:
+//  1) Find the path to the file
+//  2) Load that into an instance of `Data`
+//  3) Pass the instance through a JSONDecoder()
+
+// A better way is to write an Extension on `Bundle`, so that it's done
+//  all in one place!
+
+struct DecodableJSONView : View {
+    let astronauts = Bundle.main.decodeAstronauts("astronauts.json")
+    
+    var body : some View {
+        VStack {
+            Text(String(astronauts.count))
+        }
+    }
+}
